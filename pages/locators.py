13270d73e8c1cd00818220b65d45815f1e858113
Yegor <nobodyknowmymail@gmail.com>
2020-08-06 14:37:55 +0300

    Added new page, locators. Updated existing test with parametrize and xfail

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,13 @@ class LoginPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    ADDED_TO_CART_MSG = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    TOTAL_IN_BASKET_MSG = (
+        By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong"
+    )
